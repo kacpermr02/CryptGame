aac51096ac34fc3feb07e8d20ed8903859e609ea
kacpermr02 <168301722+kacpermr02@users.noreply.github.com>
2025-11-20 18:46:22 +0100

    Game 8

---
+++ CryptGame/Source/CryptRaider/TriggerComponent.cpp
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TriggerComponent.h"
+
+UTriggerComponent::UTriggerComponent()
+{
+    PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UTriggerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+ void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+ {
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    AActor* Actor = GetAccaptableActor();
+    if (Actor != nullptr)
+    {
+        UPrimitiveComponent* Component = Cast<UPrimitiveComponent>(Actor->GetRootComponent());
+        if (Component != nullptr)
+        {
+            Component->SetSimulatePhysics(false);
+        }
+        Actor->AttachToComponent(this, FAttachmentTransformRules::KeepWorldTransform);
+        Mover->SetShouldMove(true);
+    }
+    else
+    {
+        Mover->SetShouldMove(false);
+    }
+    
+ }
+
+void UTriggerComponent::SetMover(UMover* NewMover)
+{
+    Mover = NewMover;
+}
+
+AActor* UTriggerComponent::GetAccaptableActor() const
+{
+    TArray<AActor*> Actors;
+    GetOverlappingActors(Actors);
+
+    for (AActor* Actor : Actors)
+    {
+        bool HasAccaptableTag = Actor->ActorHasTag(AccaptableActorTag);
+        bool IsGrabbed = Actor->ActorHasTag("Grabbed");
+        if (HasAccaptableTag && !IsGrabbed)
+        {
+            return Actor;
+        }
+    }
+
+    return nullptr;
+}
